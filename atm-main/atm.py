cdc332af9c47ee8496237c5132546247ad0dbec7
BlownCapacitor <52745475+BlownCapacitor@users.noreply.github.com>
2021-01-31 09:59:20 +0530

    Add files via upload

---
+++ atm-main/atm.py
@@ -0,0 +1,14 @@
+class Atm(object):
+    def __init__  (self, cardNumber, cardPin ):
+        self.cardNumber = cardNumber
+        self.cardPin  = cardPin
+     
+    def withdrawal(self):
+        amount = input("how much do you want to withdraw")
+        w = "You have withdrawn {} rupees".format(amount)
+        print(w)
+    def checkBalance(self):
+        balance = []
+        print("Balance:" ,balance)
+atm = Atm(input("Enter Card Number"),input("Enter Pin"))
+print(atm.withdrawal())
